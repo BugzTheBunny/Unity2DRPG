0aef38711a03d3cc94789bfc584eece0b2daae2a
unknown <urigami2010@gmail.com>
2023-07-14 18:51:38 +0300

    made bow to work

--- Assets/Scripts/Inventory/Weapons/Bow.cs
+++ Assets/Scripts/Inventory/Weapons/Bow.cs
@@ -5,10 +5,23 @@ using UnityEngine;
 public class Bow : MonoBehaviour, IWeapon
 {
     [SerializeField] private WeaponInfo weaponInfo;
+    [SerializeField] private GameObject arrowPrefab;
+    [SerializeField] private Transform arrowSpawnPoint;
+
+    private Animator animator;
+
+    readonly int FIRE_HASH = Animator.StringToHash("Fire");
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     public void Attack()
     {
-        Debug.Log("Bow Attack");
+
+        GameObject arrow = Instantiate(arrowPrefab, arrowSpawnPoint.position, ActiveWeapon.Instance.transform.rotation);
+        animator.SetTrigger(FIRE_HASH);
     }
 
     public WeaponInfo GetWeaponInfo()
