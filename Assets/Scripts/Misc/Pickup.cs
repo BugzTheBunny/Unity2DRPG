427ab9a60c38c0d7451e3d0fe621614d26496dd9
unknown <urigami2010@gmail.com>
2023-07-30 11:58:53 +0300

    added coins

---
+++ Assets/Scripts/Misc/Pickup.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    [SerializeField] private float pikcUpDistance = 5f;
+    [SerializeField] private float moveSpeed = 2f;
+    [SerializeField] private float moveSpeedAcceleration = .5f;
+    [SerializeField] private AnimationCurve animCurve;
+    [SerializeField] private float heightY = 1.5f;
+    [SerializeField] private float popDuration = 1f;
+
+    private Rigidbody2D rb;
+
+    private Vector3 moveDirection;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        Debug.Log("Spawn!");
+        StartCoroutine(AnimCurveSpawnRoutine());
+        Debug.Log("Spawn!");
+    }
+
+    private void Update()
+    {
+        Vector3 playerPos = PlayerController.Instance.transform.position;
+
+        if (Vector3.Distance(transform.position, playerPos) < pikcUpDistance)
+        {
+            moveDirection = (playerPos - transform.position).normalized;
+            moveSpeed += moveSpeedAcceleration;
+        } else {
+            moveDirection = Vector3.zero;
+            moveSpeed = 0f;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = moveDirection * moveSpeed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.GetComponent<PlayerController>())
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private IEnumerator AnimCurveSpawnRoutine()
+    {
+        Vector2 startPoint = transform.position;
+
+        float randomX = transform.position.x + Random.Range(-2f, 2f);
+        float randomY = transform.position.y + Random.Range(-1f, 1f);
+
+        Vector2 endPoint = new Vector2(randomX, randomY);
+
+        float timePassed = 0f;
+
+        while (timePassed < popDuration)
+        {
+            timePassed += Time.deltaTime;
+            float linearT = timePassed / popDuration;
+            float heightT = animCurve.Evaluate(linearT);
+            float height = Mathf.Lerp(0f, heightY, heightT);
+
+            transform.position = Vector2.Lerp(startPoint, endPoint, linearT) + new Vector2(0f, height);
+            yield return null;
+        }
+    }
+}
