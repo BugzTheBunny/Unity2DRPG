edf4c3392914c98d2ebf4a5ec752b29d9f5db41f
unknown <urigami2010@gmail.com>
2023-07-31 12:53:54 +0300

    changed default cursor

---
+++ Assets/Scripts/Misc/CursorManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorManager : MonoBehaviour
+{
+    void Start()
+    {
+        Cursor.visible = false;
+        if (Application.isPlaying )
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+        Cursor.lockState = CursorLockMode.Confined;
+
+
+    }
+
+    void Update()
+    {
+        Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = cursorPos;
+
+        if (!Application.isPlaying ) { return; }
+        Cursor.visible = false;
+    }
+}
